ca1697f2d6a9e4a50f6b351dced13dff05819f34
edsondearaujo <edsondearaujofga@gmail.com>
2021-01-14 23:37:01 -0300

    feat(#11): about_var_let_const

---
+++ src/var_let_const.js
@@ -0,0 +1,12 @@
+ /* var é a palavra reservada da linguagem que mostra que estou alocando
+ um espaço na moemŕoria do computador para guardar o identificador, que seria minha "variável_A".
+
+O símbolo de igualdade (=), significa que estou atribuindo um valo para minha variável (variavel_A).
+ */
+var variavel_A = 10;
+let variavel_B = 20;
+
+const pi = 3.15; //constante o valor não muda
+
+console.log(variavel_A);
+console.log(pi);
