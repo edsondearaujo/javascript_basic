b3daeda2c974d2e11af5f6168cd0d6f00502ff82
edsondearaujo <edsondearaujofga@gmail.com>
2021-01-15 23:35:52 -0300

    feat(#12): numbers

---
+++ src/numeros.js
@@ -0,0 +1,11 @@
+const prova_1 = 5.2;
+const prova_2 = 3.0;
+
+console.log(prova_1, prova_2);
+
+const peso_P1 = 1;
+const peso_P2 = 2;
+
+const nota_final = prova_1 * peso_P1 + prova_2 * peso_P2;
+
+console.log(nota_final);
