09dd90d882cbbe224e96658740f1795edd60c0fe
edsondearaujo <edsondearaujofga@gmail.com>
2021-01-16 23:38:21 -0300

    feat(#12): methods

--- src/numeros.js
+++ src/numeros.js
@@ -6,8 +6,10 @@ console.log(prova_1, prova_2);
 const peso_P1 = 2;
 const peso_P2 = 3;
 
-const nota_final = prova_1 * peso_P1 + prova_2 * peso_P2;
-const media = nota_final / (peso_P1 + peso_P2)
+const nota_final = (prova_1 * peso_P1) + (prova_2 * peso_P2);
+const media = nota_final / (peso_P1 + peso_P2);
 
-console.log(nota_final);
-console.log(media);
+console.log("Nota final: ", nota_final);
+console.log("Média: ", media.toFixed(2));
+console.log("Binário: ", media.toString(2));
+console.log("Tipo: ", typeof media);
