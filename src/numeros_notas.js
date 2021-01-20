4e59547552a9a794835f8e2234310a17b97315bb
edsondearaujo <edsondearaujofga@gmail.com>
2021-01-20 14:51:46 -0300

    feat(#12): attention about number

---
+++ src/numeros_notas.js
@@ -0,0 +1,12 @@
+console.log(7/0); // um número dividido poe zero dente ao infinito.
+
+// Nesse caso o Javascript entende que existe um número dentro da string e realiza a operação.
+console.log("15" / 3);
+
+/*Em algumas linguagens retorna o número dois duas vezes,
+aqui no javascript não funciona. retorna um NaN(Not a Number)*/
+console.log("show!" * 2);
+
+/* Existe uma especificação que gera uma imprecisão de ponto flutuante no javascript.
+Procurar a especificação*/
+console.log(0.1 + 0.7);
