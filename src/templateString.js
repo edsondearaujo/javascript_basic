ebe58dc0c07f14eaa40bc8ce82f6518f7f8bb2ae
edsondearaujo <edsondearaujofga@gmail.com>
2021-01-21 11:30:22 -0300

    feat(#16): template string

---
+++ src/templateString.js
@@ -0,0 +1,7 @@
+const produto = 'Cogumelos';
+const concatenacao = 'Você trabalha com qual produto? ' + produto;
+
+const template = `
+Qual produto você trabalha? 
+${produto}`;
+console.log(concatenacao, template);
