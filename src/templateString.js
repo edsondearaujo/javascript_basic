788432ec994c00ed90918b6a0f168dcc8350f51f
edsondearaujo <edsondearaujofga@gmail.com>
2021-01-21 12:10:21 -0300

    feat(#16): split, csv format

--- src/templateString.js
+++ src/templateString.js
@@ -5,3 +5,10 @@ const template = `
 Qual produto você trabalha? 
 ${produto}`;
 console.log(concatenacao, template);
+
+// Expressões
+console.log(`2 * 8 = ${2*8}`);
+
+// chamando uma função usando "Template String"
+const up = texto_entrada => texto_entrada.toUpperCase();
+console.log(`Ei! vc aí. ${up('cuidado')}!`)
